36dbc1482999296f3567ad733e8d799d6f06b996
magicFeirl <2100709458@qq.com>
2022-07-08 15:29:32 +0800

    untrack config file

--- app/gelbooru/config.py
+++
@@ -1,8 +0,0 @@
-# 标签列表
-GELBOORU_TAGS = []
-
-# 爬取页数
-GELBOORU_PAGE_NUM = 1
-
-# 图片质量 file_url: 原图；sample_url：缩略图
-GELBOORU_IMAGE_QUALITY = 'file_url'  # or sample_url
--- app/twitter/config.py
+++
@@ -1,8 +0,0 @@
-# key 和 token 的获取见 twitter 文档 dev 部分
-TWITTER_API_KEY = ''
-TWITTER_API_KEY_SECRET = ''
-TWITTER_ACCESS_TOKEN = ''
-TWITTER_ACCESS_SECRET = ''
-
-# 目标列表Id
-TWITTER_LIST_ID = []
