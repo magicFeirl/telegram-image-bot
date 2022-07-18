3f65be1c00d7972e2e153d56c8a1cb06c3e70b97
magicFeirl <2100709458@qq.com>
2022-07-18 14:04:14 +0800

    add config file

---
+++ config.py
@@ -0,0 +1,11 @@
+# bot token
+TOKEN = 'xxxx'
+
+# 消息发送失败的最大重试次数
+MESSAGE_MAX_RETRY = 12
+
+# group/user/channel id 列表
+CHAD_ID_LIST = ['1234567']
+
+# 代理地址（本地使用需要设置）
+PROXY = None  # 'http://127.0.0.1:1081'
